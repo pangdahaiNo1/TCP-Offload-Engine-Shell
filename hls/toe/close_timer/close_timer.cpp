5d2c23fb1d1009ee7b9a295f387348c01bf744c2
zelin <wangzelin20s@ict.ac.cn>
2022-04-24 10:02:40 +0800

    feat(close-timer): impl close-timer,
    The close-timer is used for TIME_WAIT state, when the TOE is active
    close

---
+++ hls/toe/close_timer/close_timer.cpp
@@ -0,0 +1,58 @@
+#include "close_timer.hpp"
+
+using namespace hls;
+
+/** @ingroup close_timer
+ *  Reads in Session-IDs, the corresponding is kept in the TIME-WAIT state for 60s before
+ *  it gets closed by writing its ID into the closeTimerReleaseFifo.
+ *  @param[in]		rx_eng_to_timer_set_ctimer, FIFO containing Session-ID of the sessions which are
+ * in the TIME-WAIT state
+ *  @param[out]		ctimer_to_sttable_release_state, write Sessions which are closed into this FIFO
+ */
+void                 close_timer(stream<TcpSessionID> &rx_eng_to_timer_set_ctimer,
+                                 stream<TcpSessionID> &ctimer_to_sttable_release_state) {
+#pragma HLS PIPELINE II = 1
+
+#pragma HLS DATA_PACK variable = rx_eng_to_timer_set_ctimer
+#pragma HLS DATA_PACK variable = ctimer_to_sttable_release_state
+
+  static CloseTimerEntry close_timer_table[TCP_MAX_SESSIONS];
+#pragma HLS RESOURCE variable = close_timer_table core     = RAM_T2P_BRAM
+#pragma HLS DATA_PACK                             variable = close_timer_table
+#pragma HLS DEPENDENCE variable                            = close_timer_table inter false
+
+  static TcpSessionID ctimer_cur_session_id  = 0;
+  static TcpSessionID ctimer_set_session_id  = 0;
+  static TcpSessionID ctimer_prev_session_id = 0;
+  static bool         ctimer_wait_for_write  = false;
+  // TcpSessionID session_id;
+
+  if (ctimer_wait_for_write) {
+    if (ctimer_set_session_id != ctimer_prev_session_id) {
+      close_timer_table[ctimer_set_session_id].time   = TIME_60s;
+      close_timer_table[ctimer_set_session_id].active = true;
+      ctimer_wait_for_write                           = false;
+    }
+    ctimer_prev_session_id--;
+  } else if (!rx_eng_to_timer_set_ctimer.empty()) {
+    rx_eng_to_timer_set_ctimer.read(ctimer_set_session_id);
+    ctimer_wait_for_write = true;
+  } else {
+    ctimer_prev_session_id = ctimer_cur_session_id;
+    // Check if 0, otherwise decrement
+    if (close_timer_table[ctimer_cur_session_id].active) {
+      if (close_timer_table[ctimer_cur_session_id].time > 0) {
+        close_timer_table[ctimer_cur_session_id].time -= 1;
+      } else {
+        close_timer_table[ctimer_cur_session_id].time   = 0;
+        close_timer_table[ctimer_cur_session_id].active = false;
+        ctimer_to_sttable_release_state.write(ctimer_cur_session_id);
+      }
+    }
+
+    ctimer_cur_session_id++;
+    if (ctimer_cur_session_id == TCP_MAX_SESSIONS) {
+      ctimer_cur_session_id = 0;
+    }
+  }
+}
---
+++ hls/toe/close_timer/close_timer.hpp
@@ -0,0 +1,17 @@
+#include "toe/tcp_conn.hpp"
+
+using namespace hls;
+
+/** @ingroup close_timer
+ *
+ */
+struct CloseTimerEntry {
+  ap_uint<32> time;
+  bool        active;
+};
+
+/** @defgroup close_timer Close Timer
+ *
+ */
+void close_timer(stream<TcpSessionID> &rx_eng_to_timer_set_ctimer,
+                 stream<TcpSessionID> &ctimer_to_sttable_release_state);
