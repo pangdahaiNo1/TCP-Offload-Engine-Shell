63afa80a95b2b01725f0f5bb007cface24bf3082
zelin <wangzelin20s@ict.ac.cn>
2022-04-24 09:46:43 +0800

    feat(probe-timer)
    Probe the other side receive window if current window is zero

---
+++ hls/toe/probe_timer/probe_timer.cpp
@@ -0,0 +1,76 @@
+#include "probe_timer.hpp"
+
+using namespace hls;
+
+/** @ingroup probe_timer
+ *  Reads in the Session-ID and activates. a timer with an interval of 0.05 seconds. When the timer
+ *times out a RT Event is fired to the @ref tx_engine. In case of a zero-window (or too small
+ *window) an RT Event will generate a packet without payload which is the same as a probing packet.
+ *	@param[in]		tx_eng_to_timer_set_ptimer
+ *	@param[out]		ptimer_to_event_eng_set_event
+ */
+void                  probe_timer(stream<TcpSessionID> &rx_eng_to_timer_clear_ptimer,
+                                  stream<TcpSessionID> &tx_eng_to_timer_set_ptimer,
+                                  stream<Event> &       ptimer_to_event_eng_set_event) {
+#pragma HLS DATA_PACK variable = tx_eng_to_timer_set_ptimer
+#pragma HLS DATA_PACK variable = ptimer_to_event_eng_set_event
+
+#pragma HLS PIPELINE II = 1
+
+  static ProbeTimerEntry probe_timer_table[TCP_MAX_SESSIONS];
+#pragma HLS RESOURCE variable = probe_timer_table core     = RAM_T2P_BRAM
+#pragma HLS DATA_PACK                             variable = probe_timer_table
+#pragma HLS DEPENDENCE variable                            = probe_timer_table inter false
+
+  static TcpSessionID ptimer_cur_session_id  = 0;
+  static TcpSessionID ptimer_upd_session_id  = 0;
+  static TcpSessionID ptimer_prev_session_id = 0;
+  static bool         ptimer_wait_for_write  = false;
+
+  TcpSessionID ptimer_cur_check_or_clear_session_id;
+  bool         ptimer_cur_id_is_clear = false;
+
+  if (ptimer_wait_for_write) {
+    if (ptimer_upd_session_id != ptimer_prev_session_id) {
+      probe_timer_table[ptimer_upd_session_id].time   = TIME_50ms;
+      probe_timer_table[ptimer_upd_session_id].active = true;
+      ptimer_wait_for_write                           = false;
+    }
+    ptimer_prev_session_id--;
+  } else if (!tx_eng_to_timer_set_ptimer.empty()) {
+    tx_eng_to_timer_set_ptimer.read(ptimer_upd_session_id);
+    ptimer_wait_for_write = true;
+  } else {
+    ptimer_cur_check_or_clear_session_id = ptimer_cur_session_id;
+
+    if (!rx_eng_to_timer_clear_ptimer.empty()) {
+      rx_eng_to_timer_clear_ptimer.read(ptimer_cur_check_or_clear_session_id);
+      ptimer_cur_id_is_clear = true;
+    } else {
+      ptimer_cur_session_id++;
+      if (ptimer_cur_session_id == TCP_MAX_SESSIONS) {
+        ptimer_cur_session_id = 0;
+      }
+    }
+
+    // Check if 0, otherwise decrement
+    if (probe_timer_table[ptimer_cur_check_or_clear_session_id].active &&
+        !ptimer_to_event_eng_set_event.full()) {
+      if (probe_timer_table[ptimer_cur_check_or_clear_session_id].time == 0 ||
+          ptimer_cur_id_is_clear) {
+        probe_timer_table[ptimer_cur_check_or_clear_session_id].time   = 0;
+        probe_timer_table[ptimer_cur_check_or_clear_session_id].active = false;
+        // It's not an RT, we want to resume TX
+#if !(TCP_NODELAY)
+        ptimer_to_event_eng_set_event.write(Event(TX, ptimer_cur_check_or_clear_session_id));
+#else
+        ptimer_to_event_eng_set_event.write(Event(RT, ptimer_cur_check_or_clear_session_id));
+#endif
+        ptimer_cur_id_is_clear = false;
+      } else {
+        probe_timer_table[ptimer_cur_check_or_clear_session_id].time -= 1;
+      }
+    }
+    ptimer_prev_session_id = ptimer_cur_check_or_clear_session_id;
+  }
+}
---
+++ hls/toe/probe_timer/probe_timer.hpp
@@ -0,0 +1,19 @@
+
+#include "toe/tcp_conn.hpp"
+
+using namespace hls;
+
+/** @ingroup probe_timer
+ *
+ */
+struct ProbeTimerEntry {
+  ap_uint<32> time;
+  bool        active;
+};
+
+/** @defgroup probe_timer Probe Timer
+ *
+ */
+void probe_timer(stream<TcpSessionID> &rx_eng_to_timer_clear_ptimer,
+                 stream<TcpSessionID> &tx_eng_to_timer_set_ptimer,
+                 stream<Event> &       ptimer_to_event_eng_set_event);
