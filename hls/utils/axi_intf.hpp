48ec322b4f9ea8f2e0eaa135b9e3c2f53e457bef
zelin <wangzelin20s@ict.ac.cn>
2022-05-23 11:31:04 +0800

    refector(NetAXIS): add the AXI stream port for VitisHLS

--- hls/utils/axi_intf.hpp
+++ hls/utils/axi_intf.hpp
@@ -1,7 +1,7 @@
 #ifndef _AXI_INTF_HPP_
 #define _AXI_INTF_HPP_
 #include "ap_int.h"
-
+#include "ap_axi_sdata.h"
 #include <hls_stream.h>
 
 #define NET_TDATA_WIDTH 512
@@ -19,17 +19,19 @@ typedef ap_uint<NET_TDEST_WIDTH>     NetAXISDest;
 typedef ap_uint<NET_TDATA_WIDTH / 8> NetAXISKeep;
 
 const NetAXISDest INVALID_TDEST = 0xF;
-struct NetAXIS {
+struct NetAXISWord {
   NetAXISData data;
   // NetAXISId   id;
   // NetAXISUser user;
   NetAXISDest dest;
   NetAXISKeep keep;
   ap_uint<1>  last;
-  // NetAXIS() : data(0), id(0), user(0), dest(0), keep(0), last(0) {}
-  NetAXIS() : data(0), dest(0), keep(0), last(1) {}
-  NetAXIS(NetAXISData data, NetAXISDest dest, NetAXISKeep keep, ap_uint<1> last)
+  // NetAXISWord() : data(0), id(0), user(0), dest(0), keep(0), last(0) {}
+  NetAXISWord() : data(0), dest(0), keep(0), last(1) {}
+  NetAXISWord(NetAXISData data, NetAXISDest dest, NetAXISKeep keep, ap_uint<1> last)
       : data(data), dest(dest), keep(keep), last(last) {}
 };
+
+typedef ap_axiu<NET_TDATA_WIDTH, 0, 0, NET_TDEST_WIDTH> NetAXIS;
 
 #endif  // _AXI_INTF_HPP_
