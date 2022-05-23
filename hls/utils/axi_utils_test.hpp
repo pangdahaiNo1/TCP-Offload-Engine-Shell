a91cf0371269f212f8b108defe9d286f5447b048
zelin <wangzelin20s@ict.ac.cn>
2022-05-23 16:33:28 +0800

    refector(AXIS2file): change the stream struct to NetAXISWord

--- hls/utils/axi_utils_test.hpp
+++ hls/utils/axi_utils_test.hpp
@@ -9,14 +9,14 @@
 using namespace std;
 using hls::stream;
 
-int LoadNetAXISFromFile(stream<NetAXIS> &net_axis_stream, const string &file_name) {
+int LoadNetAXISFromFile(stream<NetAXISWord> &net_axis_stream, const string &file_name) {
   std::ifstream axis_infile(file_name);
   if (!axis_infile.is_open()) {
     std::cout << "Error: could not open tcp input file." << std::endl;
     return -1;
   }
-  NetAXIS one_data;
-  string  one_data_data, one_data_id, one_data_user, one_data_dest, one_data_keep, one_data_last;
+  NetAXISWord one_data;
+  string one_data_data, one_data_id, one_data_user, one_data_dest, one_data_keep, one_data_last;
   while (axis_infile >> one_data_data >> one_data_dest >> one_data_keep >> one_data_last) {
     one_data.data = NetAXISData(one_data_data.c_str(), 16);
     // one_data.id   = NetAXISId(one_data_id.c_str(), 16);
@@ -31,10 +31,10 @@ int LoadNetAXISFromFile(stream<NetAXIS> &net_axis_stream, const string &file_nam
   return 0;
 }
 
-void SaveNetAXISToFile(stream<NetAXIS> &net_axis_stream, const string &file_name) {
+void SaveNetAXISToFile(stream<NetAXISWord> &net_axis_stream, const string &file_name) {
   std::ofstream axis_outfile(file_name);
   while (!net_axis_stream.empty()) {
-    NetAXIS one_data;
+    NetAXISWord one_data;
     net_axis_stream.read(one_data);
     axis_outfile << one_data.data.to_string(16);
     axis_outfile << " ";
@@ -53,16 +53,16 @@ void SaveNetAXISToFile(stream<NetAXIS> &net_axis_stream, const string &file_name
   axis_outfile.close();
 }
 
-int ComparePacpPacketsWithGolden(stream<NetAXIS> &dut_packets,
-                                 stream<NetAXIS> &golden_packets,
-                                 bool             debug_all_bytes) {
+int ComparePacpPacketsWithGolden(stream<NetAXISWord> &dut_packets,
+                                 stream<NetAXISWord> &golden_packets,
+                                 bool                 debug_all_bytes) {
   int  wordCount          = 0;
   int  packets            = 0;
   int  error_packets      = 0;
   bool error_packets_flag = false;
   while (!dut_packets.empty()) {
-    NetAXIS curr_word;
-    NetAXIS golden_word;
+    NetAXISWord curr_word;
+    NetAXISWord golden_word;
     dut_packets.read(curr_word);
     golden_packets.read(golden_word);
     int error = 0;
@@ -112,6 +112,14 @@ int ComparePacpPacketsWithGolden(stream<NetAXIS> &dut_packets,
   cout << "Compared packets " << dec << packets << endl;
   return error_packets;
   ;
+}
+
+void NetAXIStreamToNetAXIStreamWord(stream<NetAXIS> &    net_axis_stream,
+                                    stream<NetAXISWord> &net_axis_word_stream) {
+  stream<NetAXISWord> ret_stream;
+  while (!net_axis_stream.empty()) {
+    net_axis_word_stream.write(net_axis_stream.read());
+  }
 }
 
 #endif
