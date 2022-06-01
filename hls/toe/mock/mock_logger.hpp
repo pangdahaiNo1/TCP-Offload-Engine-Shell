18634eda03e91797e54315253ac045dc8bff733e
zelin <wangzelin20s@ict.ac.cn>
2022-06-01 16:05:50 +0800

    feat(mock-logger): add logger for output inner state
    Usage: add header files in <module_top> file
    ```
    extern MockLogger logger;
    ```
    then assign `logger` in test file

---
+++ hls/toe/mock/mock_logger.hpp
@@ -0,0 +1,47 @@
+#ifndef _MOCK_LOGGER_HPP_
+#define _MOCK_LOGGER_HPP_
+
+#include "toe/toe_conn.hpp"
+#include "utils/pcap/pcap_to_stream.hpp"
+
+#include <fstream>
+#include <string>
+
+using std::ofstream;
+using std::string;
+
+#ifndef __SYNTHESIS__
+class MockLogger {
+public:
+  uint64_t sim_cycle;
+  ofstream output_stream;
+
+  MockLogger(string log_file) {
+    sim_cycle = 0;
+    output_stream.open(log_file);
+    if (!output_stream) {
+      std::cout << "Error: could not open test output file." << std::endl;
+    }
+  }
+
+  inline void InfoOutput(string description, string state, bool state_in_new_line) {
+    if (!state_in_new_line) {
+      output_stream << "Cycle " << sim_cycle << ": " << description << "\t" << state << "\n";
+    } else {
+      output_stream << "Cycle " << sim_cycle << ": " << description << "\n"
+                    << "\t\t" << state << "\n";
+    }
+  }
+
+  void IncreaseSimCycle() { sim_cycle++; }
+  void SetSimCycle(uint64_t cycle) { sim_cycle = cycle; };
+};
+#else
+
+class MockLogger {
+public:
+  inline void InfoOutput(const char *fmt...) {}
+};
+#endif
+
+#endif  // _MOCK_LOGGER_HPP_
