5b20f58b34f7f077e12a0eb928a9ad9c6dedd1e5
zelin <wangzelin20s@ict.ac.cn>
2022-06-01 18:00:59 +0800

    test(mock-cam): add logger for mock-cam

--- hls/toe/mock/mock_cam.hpp
+++ hls/toe/mock/mock_cam.hpp
@@ -1,24 +1,23 @@
 #ifndef _MOCK_CAM_HPP_
 #define _MOCK_CAM_HPP_
+#include "toe/mock/mock_logger.hpp"
 #include "toe/toe_conn.hpp"
 
-#include <fstream>
-#include <iostream>
-#include <algorithm>
 #include <map>
-using hls::stream;
-using namespace std;
 
+using hls::stream;
+using std::cerr;
+using std::endl;
+using std::map;
 class MockCam {
 public:
-  map<ThreeTuple, TcpSessionID> _mock_cam;
-  MockCam() { _mock_cam = map<ThreeTuple, TcpSessionID>(); }
-  void MockCamIntf(std::ofstream &                out_stream,
+  std::map<ThreeTuple, TcpSessionID> _mock_cam;
+  MockCam() { _mock_cam = std::map<ThreeTuple, TcpSessionID>(); }
+  void MockCamIntf(MockLogger &                   logger,
                    stream<RtlSLookupToCamLupReq> &slookup_to_cam_lup_req,
                    stream<RtlCamToSlookupLupRsp> &cam_to_slookup_lup_rsp,
                    stream<RtlSlookupToCamUpdReq> &slookup_to_cam_upd_req,
-                   stream<RtlCamToSlookupUpdRsp> &cam_to_slookup_upd_rsp,
-                   int                            sim_cycle) {
+                   stream<RtlCamToSlookupUpdRsp> &cam_to_slookup_upd_rsp) {
     RtlSLookupToCamLupReq lookup_req;
     RtlCamToSlookupLupRsp lookup_rsp;
     RtlSlookupToCamUpdReq update_req;
@@ -28,8 +27,7 @@ public:
 
     if (!slookup_to_cam_lup_req.empty()) {
       slookup_to_cam_lup_req.read(lookup_req);
-      out_stream << "Cycle " << std::dec << sim_cycle << ": Slookup to CAM Lookup Req\t";
-      out_stream << lookup_req.to_string() << "\n";
+      logger.Info("Slookup to CAM Lookup Req", lookup_req.to_string(), false);
 
       iter = _mock_cam.find(lookup_req.key);
       if (iter != _mock_cam.end())  // hit
@@ -39,16 +37,13 @@ public:
         lookup_rsp = RtlCamToSlookupLupRsp(false, lookup_req.source);
       }
       cam_to_slookup_lup_rsp.write(lookup_rsp);
-      out_stream << "Cycle " << std::dec << sim_cycle << ": CAM to Slookup Lookup Rsp\t";
-      out_stream << lookup_rsp.to_string() << "\n";
+      logger.Info("CAM to Slookup Lookup Rsp", lookup_rsp.to_string(), false);
     }
 
     if (!slookup_to_cam_upd_req.empty()) {
       slookup_to_cam_upd_req.read(update_req);
 
-      out_stream << "Cycle " << std::dec << sim_cycle << ": Slookup to CAM Update Req\t";
-      out_stream << update_req.to_string() << "\n";
-
+      logger.Info("Slookup to CAM Update Req", update_req.to_string(), false);
       iter = _mock_cam.find(update_req.key);
       if (iter == _mock_cam.end()) {
         if (update_req.op == INSERT) {
@@ -57,19 +52,18 @@ public:
           cout << "Current Update session: " << update_req.value << endl;
 
         } else {
-          cerr << "delete a non-existing one" << endl;
+          std::cerr << "delete a non-existing one" << endl;
         }
       } else {  // found in map
         if (update_req.op == INSERT) {
-          cerr << "insert a existing one" << endl;
+          std::cerr << "insert a existing one" << endl;
         } else {
           _mock_cam.erase(update_req.key);
           update_rsp = RtlCamToSlookupUpdRsp(update_req.value, DELETE, update_req.source);
         }
       }
       cam_to_slookup_upd_rsp.write(update_rsp);
-      out_stream << "Cycle " << std::dec << sim_cycle << ": CAM to Slookup Update Rsp\t";
-      out_stream << update_rsp.to_string() << "\n";
+      logger.Info("CAM to Slookup Update Rsp", update_rsp.to_string(), false);
     }
   }
 };
