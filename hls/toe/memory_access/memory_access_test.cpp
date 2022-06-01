c532d7e8c558c0b525e9dfbecbc434dd0fc9fe88
zelin <wangzelin20s@ict.ac.cn>
2022-06-01 17:49:38 +0800

    test(mem-access): add out top logger for mem-access

--- hls/toe/memory_access/memory_access_test.cpp
+++ hls/toe/memory_access/memory_access_test.cpp
@@ -1,68 +1,56 @@
-
 #include "memory_access.hpp"
+#include "toe/mock/mock_logger.hpp"
 #include "toe/mock/mock_memory.hpp"
 #include "toe/mock/mock_toe.hpp"
 #include "utils/axi_utils_test.hpp"
 #include "utils/pcap/pcap_to_stream.hpp"
 using namespace hls;
 
-void EmptyTxWriteDataFifos(std::ofstream &       out_stream,
+void EmptyTxWriteDataFifos(MockLogger &          logger,
                            stream<NetAXIS> &     mover_mem_data_out,
-                           stream<DataMoverCmd> &mover_mem_cmd_out,
-                           int                   sim_cycle) {
+                           stream<DataMoverCmd> &mover_mem_cmd_out) {
   DataMoverCmd cmd_out;
   NetAXIS      data_out;
 
   while (!mover_mem_cmd_out.empty()) {
     mover_mem_cmd_out.read(cmd_out);
-    out_stream << "Cycle " << std::dec << sim_cycle << ": Tx App write cmd: \t";
-    out_stream << cmd_out.to_string() << "\n";
+    logger.Info("Tx App write cmd ", cmd_out.to_string(), false);
   }
   while (!mover_mem_data_out.empty()) {
     mover_mem_data_out.read(data_out);
-    out_stream << "Cycle " << std::dec << sim_cycle << ": Tx App write \t";
-    out_stream << dec << KeepToLength(data_out.keep) << " Bytes word to mem\n";
+    logger.Info(
+        "Tx App write ", to_string(KeepToLength(data_out.keep)) + " Bytes word to mem", false);
   }
 }
 
-void EmptyTxReadCmdFifos(std::ofstream &                     out_stream,
+void EmptyTxReadCmdFifos(MockLogger &                        logger,
                          stream<MemBufferRWCmdDoubleAccess> &mem_buffer_double_access,
-                         stream<DataMoverCmd> &              mover_mem_cmd_out,
-                         int                                 sim_cycle) {
+                         stream<DataMoverCmd> &              mover_mem_cmd_out) {
   DataMoverCmd               cmd_out;
   MemBufferRWCmdDoubleAccess double_access;
 
   while (!mover_mem_cmd_out.empty()) {
     mover_mem_cmd_out.read(cmd_out);
-    out_stream << "Cycle " << std::dec << sim_cycle << ": Tx eng read mem cmd: \t";
-    out_stream << cmd_out.to_string() << "\n";
+    logger.Info("Tx eng read mem cmd", cmd_out.to_string(), false);
   }
   while (!mem_buffer_double_access.empty()) {
     mem_buffer_double_access.read(double_access);
-    out_stream << "Cycle " << std::dec << sim_cycle << ": Tx eng read mem double access check \t";
-    out_stream << double_access.to_string() << "\n";
+    logger.Info("Tx eng read mem double access check", double_access.to_string(), false);
   }
 }
 
-void EmptyTxReadDataFifos(std::ofstream &  out_stream,
-                          stream<NetAXIS> &to_tx_eng_read_data,
-                          int              sim_cycle) {
+void EmptyTxReadDataFifos(MockLogger &logger, stream<NetAXIS> &to_tx_eng_read_data) {
   NetAXIS data_out;
 
   while (!to_tx_eng_read_data.empty()) {
     to_tx_eng_read_data.read(data_out);
-    out_stream << "Cycle " << std::dec << sim_cycle << ": Tx Eng Read \t";
-    out_stream << dec << KeepToLength(data_out.keep) << " Bytes word from mem\n";
+    logger.Info(
+        "Tx eng read ", to_string(KeepToLength(data_out.keep)) + " Bytes word from mem", false);
   }
 }
 
 void TestTxWriteToMem(stream<NetAXIS> &input_tcp_packets) {
-  std::ofstream outputFile;
-
-  outputFile.open("./out_write_data.dat");
-  if (!outputFile) {
-    std::cout << "Error: could not open test output file." << std::endl;
-  }
+  MockLogger logger("out_write_data.dat");
 
   stream<NetAXISWord>  tx_app_to_mem_data_in;
   stream<DataMoverCmd> tx_app_to_mem_cmd_in;
@@ -121,18 +109,14 @@ void TestTxWriteToMem(stream<NetAXIS> &input_tcp_packets) {
     }
     TxAppWriteDataToMem(
         tx_app_to_mem_data_in, tx_app_to_mem_cmd_in, mover_mem_data_out, mover_mem_cmd_out);
-    EmptyTxWriteDataFifos(outputFile, mover_mem_data_out, mover_mem_cmd_out, sim_cycle);
+    EmptyTxWriteDataFifos(logger, mover_mem_data_out, mover_mem_cmd_out);
     sim_cycle++;
+    logger.SetSimCycle(sim_cycle);
   }
 }
 
 void TestTxEngReadCmd() {
-  std::ofstream outputFile;
-
-  outputFile.open("./out_read_cmd.dat");
-  if (!outputFile) {
-    std::cout << "Error: could not open test output file." << std::endl;
-  }
+  MockLogger logger("out_read_cmd.dat");
 
   stream<MemBufferRWCmd>             tx_eng_to_mem_cmd_in;
   stream<DataMoverCmd>               mover_read_mem_cmd_out;
@@ -158,8 +142,9 @@ void TestTxEngReadCmd() {
         break;
     }
     TxEngReadDataSendCmd(tx_eng_to_mem_cmd_in, mover_read_mem_cmd_out, mem_buffer_double_access);
-    EmptyTxReadCmdFifos(outputFile, mem_buffer_double_access, mover_read_mem_cmd_out, sim_cycle);
+    EmptyTxReadCmdFifos(logger, mem_buffer_double_access, mover_read_mem_cmd_out);
     sim_cycle++;
+    logger.SetSimCycle(sim_cycle);
   }
 }
 
@@ -178,20 +163,15 @@ void TestMockMem() {
   mem.S2MMWriteToMem(read_mem_data.read());
   mem.S2MMWriteToMem(read_mem_data.read());
   // save current mem
-  std::ofstream outputFile;
-  outputFile.open("./test_mem_dump_mem.dat");
-  outputFile << mem.DumpMockMemory();
-  outputFile.close();
+  mem.DumpMockMemoryToFile("./test_mem_dump_mem.dat");
 
   // clear mem for testing load
   mem._mock_memory.clear();
   mem.LoadMockMemoryFromFile("./test_mem_dump_mem.dat");
 
-  outputFile.open("./test_mem_dump_mem_after_load.dat");
-  outputFile << mem.DumpMockMemory();
-  outputFile.close();
+  mem.DumpMockMemoryToFile("./test_mem_dump_mem_after_load.dat");
 
-  // cout << mem.DumpMockMemory() << endl;
+  // cout << mem.DumpMockMemoryToFile() << endl;
 }
 
 void TestTxMem(stream<NetAXIS> &input_golden_packets, stream<NetAXISWord> &read_mem_packets) {
@@ -244,9 +224,7 @@ void TestTxMem(stream<NetAXIS> &input_golden_packets, stream<NetAXISWord> &read_
     sim_cycle++;
   }
   // save current mem
-  std::ofstream outputFile;
-  outputFile.open("./dump_mem.dat");
-  outputFile << mock_mem.DumpMockMemory();
+  mock_mem.DumpMockMemoryToFile("./dump_mem.dat");
 
   // tx engine read data from mem
   stream<MemBufferRWCmd>             tx_eng_to_mem_cmd_in("tx_eng_to_mem_cmd_in");
@@ -279,7 +257,7 @@ void TestTxMem(stream<NetAXIS> &input_golden_packets, stream<NetAXISWord> &read_
     sim_cycle++;
   }
 
-  // cout << mock_mem.DumpMockMemory() << endl;
+  // cout << mock_mem.DumpMockMemoryToFile() << endl;
   // cout << saddr.to_string(16) << endl;
 }
 
@@ -288,7 +266,7 @@ int main(int argc, char **argv) {
     std::cerr << "[ERROR] missing arguments " __FILE__ << " <INPUT_PCAP_FILE>" << endl;
     return -1;
   }
-  char *input_tcp_pcap_file = argv[1];
+  char *              input_tcp_pcap_file = argv[1];
   stream<NetAXIS>     input_tcp_packets("input_tcp_packets");
   stream<NetAXIS>     input_tcp_packets2("input_tcp_packets2");
   stream<NetAXIS>     input_tcp_packets3("input_tcp_packets3");
