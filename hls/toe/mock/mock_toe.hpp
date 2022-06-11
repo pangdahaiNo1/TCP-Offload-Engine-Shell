2e861a744ae04da5dfec3dfac2012aeea17f33ed
zelin <wangzelin20s@ict.ac.cn>
2022-06-11 15:51:20 +0800

    commet(toe): the comment is wrong and fix it

--- hls/toe/mock/mock_toe.hpp
+++ hls/toe/mock/mock_toe.hpp
@@ -20,7 +20,7 @@ ap_uint<32> mock_subnet_mask     = SwapByte<32>(0xFFFFFF00);
 IpAddr        mock_dst_ip_addr  = 0x0200A8C0;  // 192.168.0.2
 IpAddr        mock_src_ip_addr  = 0x0500A8C0;  // 192.168.0.5
 TcpPortNumber mock_dst_tcp_port = 0x1580;      // 32789
-TcpPortNumber mock_src_tcp_port = 0x8000;      // 120
+TcpPortNumber mock_src_tcp_port = 0x8000;      // 128
 
 FourTuple mock_tuple(mock_src_ip_addr, mock_dst_ip_addr, mock_src_tcp_port, mock_dst_tcp_port);
 FourTuple reverse_mock_tuple(mock_dst_ip_addr,
