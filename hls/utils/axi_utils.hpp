669d1a0608f179714914ac3aaed5c2035d8e5e1d
zelin <wangzelin20s@ict.ac.cn>
2022-06-22 18:38:34 +0800

    feat(axi-utils): Remove redundant functions
    1. use template parameter to generate two different MemBufferRWCmd instances

--- hls/utils/axi_utils.hpp
+++ hls/utils/axi_utils.hpp
@@ -78,8 +78,43 @@ struct SubChecksum {
 
 NetAXIS NewNetAXIS(NetAXISData data, NetAXISDest dest, NetAXISKeep keep, ap_uint<1> last);
 
-void ComputeTxSubChecksum(stream<NetAXISWord> &pkt_in, stream<SubChecksum> &sub_checksum);
-void ComputeRxSubChecksum(stream<NetAXISWord> &pkt_in, stream<SubChecksum> &sub_checksum);
+/**
+ * The template parameter is used to generate two different instances,
+ * it is not used in function
+ * in rx_engine @p is_rx = 1,
+ * in tx_engine @p is_rx = 0
+ */
+template <int is_rx>
+void ComputeSubChecksum(stream<NetAXISWord> &pkt_in, stream<SubChecksum> &sub_checksum) {
+#pragma HLS PIPELINE II = 1
+#pragma HLS INLINE   off
+  static SubChecksum tcp_checksums;
+  if (!pkt_in.empty()) {
+    NetAXISWord cur_word = pkt_in.read();
+    for (int i = 0; i < NET_TDATA_WIDTH / 16; i++) {
+#pragma HLS UNROLL
+      ap_uint<16> temp;
+      if (cur_word.keep(i * 2 + 1, i * 2) == 0x3) {
+        temp(7, 0)  = cur_word.data(i * 16 + 15, i * 16 + 8);
+        temp(15, 8) = cur_word.data(i * 16 + 7, i * 16);
+        tcp_checksums.sum[i] += temp;
+        tcp_checksums.sum[i] = (tcp_checksums.sum[i] + (tcp_checksums.sum[i] >> 16)) & 0xFFFF;
+      } else if (cur_word.keep[i * 2] == 0x1) {
+        temp(7, 0)  = 0;
+        temp(15, 8) = cur_word.data(i * 16 + 7, i * 16);
+        tcp_checksums.sum[i] += temp;
+        tcp_checksums.sum[i] = (tcp_checksums.sum[i] + (tcp_checksums.sum[i] >> 16)) & 0xFFFF;
+      }
+    }
+    if (cur_word.last == 1) {
+      sub_checksum.write(tcp_checksums);
+      for (int i = 0; i < NET_TDATA_WIDTH / 16; i++) {
+#pragma HLS UNROLL
+        tcp_checksums.sum[i] = 0;
+      }
+    }
+  }
+}
 
 void CheckChecksum(stream<SubChecksum> &sub_checksum, stream<ap_uint<16> > &valid_pkt_out);
 
